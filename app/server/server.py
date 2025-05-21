2f6fc892db22cad1394ef648870e80402a2994e3
Mahdi Kiani <mahdikiany@gmail.com>
2025-05-21 16:40:34 +0200

    add cors sample

--- app/server/server.py
+++ app/server/server.py
@@ -2,4 +2,6 @@ from fastapi_mongo_base.core import app_factory
 
 from . import config
 
-app = app_factory.create_app(settings=config.Settings())
+app = app_factory.create_app(
+    settings=config.Settings(), origins=["http://localhost:8000"]
+)
